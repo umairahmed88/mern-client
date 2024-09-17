d2117488c9118fc0562cd54e5ab1bb382f30b9ab
umairahmed88 <umairahmed_88@ymail.com>
2024-09-18 02:38:35 +0500

    updated profile page

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import { signout } from "../redux/auth/authSlices";
+import jwtDecode from "jwt-decode";
 
 export default function PrivateRoute() {
 	const { currentUser } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
 
+	if (currentUser && currentUser?.sanitizedUser?.token) {
+		try {
+			const decodedToken = jwtDecode(currentUser?.sanitizedUser?.token);
+
+			if (decodedToken.exp * 1000 < Date.now()) {
+				dispatch(signout());
+				return <Navigate to='/signin' />;
+			}
+		} catch (err) {
+			dispatch(signout());
+			return <Navigate to='/signin' />;
+		}
+	}
 	return currentUser ? <Outlet /> : <Navigate to={"/signin"} />;
 }
