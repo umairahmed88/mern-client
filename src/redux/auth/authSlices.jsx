f942c2b7a7e93a70d4c5e92cbb1da06f3e946891
umairahmed88 <umairahmed_88@ymail.com>
2024-09-17 20:39:01 +0500

    updated auth slices

--- src/redux/auth/authSlices.jsx
+++ src/redux/auth/authSlices.jsx
@@ -134,7 +134,6 @@ const authSlice = createSlice({
 			})
 			.addCase(signup.fulfilled, (state, action) => {
 				state.loading = false;
-				state.currentUser = action.payload || action.payload.auth;
 				state.message = action.payload.message;
 			})
 			.addCase(signup.rejected, (state, action) => {
