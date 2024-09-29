6c61ac569e5464636b18ca58418a2d732d140a0a
umairahmed88 <umairahmed_88@ymail.com>
2024-09-29 14:56:59 +0500

    updated products component

--- src/pages/products/AllProducts.jsx
+++ src/pages/products/AllProducts.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllProducts } from "../../redux/products/productSlices";
+
 const AllProducts = () => {
+	const { products = [] } = useSelector((state) => state.product);
+	const dispatch = useDispatch();
+
+	console.log(products);
+
+	useEffect(() => {
+		dispatch(fetchAllProducts()).unwrap();
+	}, [dispatch]);
+
 	return (
 		<div className='max-w-2xl mx-auto'>
 			<h1 className=' text-2xl font-bold m-3 text-center'>Products</h1>
