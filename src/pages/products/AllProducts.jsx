ea8658dd29531d44ed9fdb0917d5fb980905d291
umairahmed88 <umairahmed_88@ymail.com>
2024-09-29 15:38:28 +0500

    updated products page

--- src/pages/products/AllProducts.jsx
+++ src/pages/products/AllProducts.jsx
@@ -3,19 +3,36 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAllProducts } from "../../redux/products/productSlices";
 
 const AllProducts = () => {
-	const { products = [] } = useSelector((state) => state.product);
+	const { products = [], loading } = useSelector((state) => state.product);
 	const dispatch = useDispatch();
 
 	console.log(products);
 
 	useEffect(() => {
-		dispatch(fetchAllProducts()).unwrap();
+		if (products) {
+			dispatch(fetchAllProducts()).unwrap();
+		}
 	}, [dispatch]);
+
+	if (loading) return <div className=''>Loading...</div>;
+	if (!products) return <div className=''>No product available</div>;
 
 	return (
 		<div className='max-w-2xl mx-auto'>
 			<h1 className=' text-2xl font-bold m-3 text-center'>Products</h1>
 			<div className=''>Loading...</div>
+			<div className=''>
+				{products &&
+					products.map((product) => (
+						<div className='' key={product._id}>
+							<img
+								src={product.imageUrls}
+								alt=''
+								className='h-24 w-24 object-contain rounded'
+							/>
+						</div>
+					))}
+			</div>
 		</div>
 	);
 };
