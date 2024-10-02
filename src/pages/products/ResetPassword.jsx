b72bec58c063e091c89eb4929fa53a9204f0e0ea
umairahmed88 <umairahmed_88@ymail.com>
2024-10-02 13:05:12 +0500

    updated confirm password functionality

--- src/pages/products/ResetPassword.jsx
+++ src/pages/products/ResetPassword.jsx
@@ -91,7 +91,7 @@ const ResetPassword = () => {
 						className={`w-full py-2 px-4 rounded-md text-white ${
 							loading
 								? "bg-gray-400 cursor-not-allowed"
-								: "bg-indigo-600 hover:bg-indigo-700"
+								: "bg-zinc-600 hover:opacity-90"
 						}`}
 					>
 						{loading ? "Resetting..." : "Reset Password"}
