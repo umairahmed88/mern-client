359b415f6a0f9cae473a2ba6dc3d2c1697a25909
umairahmed88 <umairahmed_88@ymail.com>
2024-10-24 15:11:58 +0500

    updated checkout form

--- src/pages/checkout/CheckoutForm.jsx
+++ src/pages/checkout/CheckoutForm.jsx
@@ -92,163 +92,172 @@ const CheckoutForm = () => {
 	return (
 		<div className='container mx-auto py-10'>
 			<h1 className='text-2xl font-bold mb-6 text-center'>Checkout Form</h1>
-			<ul className=' flex flex-col gap-3'>
-				<h1 className=' text-center font-bold'>Your Shopping Cart</h1>
-				{cartItems.map((cartItem) => (
-					<li className=' flex justify-between items-center' key={cartItem._id}>
-						<img
-							src={cartItem.imageUrls}
-							alt={cartItem.name}
-							className=' h-10 w-10 rounded-full'
-						/>
-						<div className=''>
-							<p>{cartItem.name}</p>
-							<p>Items: {cartItem.quantity}</p>
-							<div className=' flex gap-3'>
-								<button
-									className=' bg-green-600 px-1.5 rounded-full font-semibold text-white'
-									onClick={() => handleIncreaseItem(cartItem._id)}
-								>
-									+
-								</button>
-								<button
-									className=' bg-red-700 px-2 rounded-full font-semibold text-white'
-									onClick={() => handleDecreaseItem(cartItem._id)}
-								>
-									-
-								</button>
-								<button
-									className=' bg-red-800 px-1.5 rounded-full font-semibold text-white'
-									onClick={() => handleDeleteItem(cartItem._id)}
-								>
-									x
-								</button>
-							</div>
+			{cartItems.length === 0 ? (
+				<div>Your cart is empty</div>
+			) : (
+				<div className=''>
+					<ul className=' flex flex-col gap-3'>
+						<h1 className=' text-center font-bold'>Your Shopping Cart</h1>
+						{cartItems.map((cartItem) => (
+							<li
+								className=' flex justify-between items-center'
+								key={cartItem._id}
+							>
+								<img
+									src={cartItem.imageUrls}
+									alt={cartItem.name}
+									className=' h-10 w-10 rounded-full'
+								/>
+								<div className=''>
+									<p>{cartItem.name}</p>
+									<p>Items: {cartItem.quantity}</p>
+									<div className=' flex gap-3'>
+										<button
+											className=' bg-green-600 px-1.5 rounded-full font-semibold text-white'
+											onClick={() => handleIncreaseItem(cartItem._id)}
+										>
+											+
+										</button>
+										<button
+											className=' bg-red-700 px-2 rounded-full font-semibold text-white'
+											onClick={() => handleDecreaseItem(cartItem._id)}
+										>
+											-
+										</button>
+										<button
+											className=' bg-red-800 px-1.5 rounded-full font-semibold text-white'
+											onClick={() => handleDeleteItem(cartItem._id)}
+										>
+											x
+										</button>
+									</div>
+								</div>
+							</li>
+						))}
+						<div className=' flex justify-end'>
+							<button
+								onClick={handleEmptyCart}
+								className=' bg-red-700 text-white px-3 rounded-lg font-bold hover:opacity-90'
+							>
+								Empty Your Cart
+							</button>
 						</div>
-					</li>
-				))}
-				<div className=' flex justify-end'>
-					<button
-						onClick={handleEmptyCart}
-						className=' bg-red-700 text-white px-3 rounded-lg font-bold hover:opacity-90'
-					>
-						Empty Your Cart
-					</button>
-				</div>
-			</ul>
-			<h1 className=' text-center text-2xl font-bold'>Order Details</h1>
-			<form onSubmit={handleSubmit} className='space-y-4'>
-				<h2 className='text-xl font-semibold'>Shipping Address</h2>
-				<div className='grid grid-cols-2 gap-4'>
-					<input
-						type='text'
-						name='addressLine1'
-						placeholder='Address Line 1'
-						value={formData.shippingAddress.addressLine1}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-						required
-					/>
-					<input
-						type='text'
-						name='addressLine2'
-						placeholder='Address Line 2'
-						value={formData.shippingAddress.addressLine2}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-					/>
-					<input
-						type='text'
-						name='city'
-						placeholder='City'
-						value={formData.shippingAddress.city}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-						required
-					/>
-					<input
-						type='text'
-						name='state'
-						placeholder='State'
-						value={formData.shippingAddress.state}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-						required
-					/>
-					<input
-						type='text'
-						name='postalCode'
-						placeholder='Postal Code'
-						value={formData.shippingAddress.postalCode}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-						required
-					/>
-					<input
-						type='text'
-						name='country'
-						placeholder='Country'
-						value={formData.shippingAddress.country}
-						onChange={(e) => handleAddressChange(e, "shippingAddress")}
-						className='border rounded p-2 w-full'
-						required
-					/>
-				</div>
+					</ul>
+					<h1 className=' text-center text-2xl font-bold'>Order Details</h1>
+					<form onSubmit={handleSubmit} className='space-y-4'>
+						<h2 className='text-xl font-semibold'>Shipping Address</h2>
+						<div className='grid grid-cols-2 gap-4'>
+							<input
+								type='text'
+								name='addressLine1'
+								placeholder='Address Line 1'
+								value={formData.shippingAddress.addressLine1}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+								required
+							/>
+							<input
+								type='text'
+								name='addressLine2'
+								placeholder='Address Line 2'
+								value={formData.shippingAddress.addressLine2}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+							/>
+							<input
+								type='text'
+								name='city'
+								placeholder='City'
+								value={formData.shippingAddress.city}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+								required
+							/>
+							<input
+								type='text'
+								name='state'
+								placeholder='State'
+								value={formData.shippingAddress.state}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+								required
+							/>
+							<input
+								type='text'
+								name='postalCode'
+								placeholder='Postal Code'
+								value={formData.shippingAddress.postalCode}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+								required
+							/>
+							<input
+								type='text'
+								name='country'
+								placeholder='Country'
+								value={formData.shippingAddress.country}
+								onChange={(e) => handleAddressChange(e, "shippingAddress")}
+								className='border rounded p-2 w-full'
+								required
+							/>
+						</div>
 
-				<h2 className='text-xl font-semibold'>Contact Information</h2>
-				<div className='grid grid-cols-2 gap-4'>
-					<input
-						type='text'
-						name='contactNum'
-						placeholder='Contact Number'
-						value={formData.contactNum}
-						onChange={handleChange}
-						className='border rounded p-2 w-full'
-						required
-					/>
-					<input
-						type='text'
-						name='altConNum'
-						placeholder='Alternate Contact Number'
-						value={formData.altConNum}
-						onChange={handleChange}
-						className='border rounded p-2 w-full'
-					/>
-				</div>
+						<h2 className='text-xl font-semibold'>Contact Information</h2>
+						<div className='grid grid-cols-2 gap-4'>
+							<input
+								type='text'
+								name='contactNum'
+								placeholder='Contact Number'
+								value={formData.contactNum}
+								onChange={handleChange}
+								className='border rounded p-2 w-full'
+								required
+							/>
+							<input
+								type='text'
+								name='altConNum'
+								placeholder='Alternate Contact Number'
+								value={formData.altConNum}
+								onChange={handleChange}
+								className='border rounded p-2 w-full'
+							/>
+						</div>
 
-				<h2 className='text-xl font-semibold'>Payment Method</h2>
-				<div className='flex space-x-4'>
-					<label className='flex items-center'>
-						<input
-							type='radio'
-							name='paymentMethod'
-							value='COD'
-							checked={formData.paymentMethod === "COD"}
-							onChange={handleChange}
-							className='mr-2'
-						/>
-						Cash on Delivery
-					</label>
-					<label className='flex items-center'>
-						<input
-							type='radio'
-							name='paymentMethod'
-							value='Stripe'
-							checked={formData.paymentMethod === "Stripe"}
-							onChange={handleChange}
-							className='mr-2'
-						/>
-						Stripe
-					</label>
-				</div>
+						<h2 className='text-xl font-semibold'>Payment Method</h2>
+						<div className='flex space-x-4'>
+							<label className='flex items-center'>
+								<input
+									type='radio'
+									name='paymentMethod'
+									value='COD'
+									checked={formData.paymentMethod === "COD"}
+									onChange={handleChange}
+									className='mr-2'
+								/>
+								Cash on Delivery
+							</label>
+							<label className='flex items-center'>
+								<input
+									type='radio'
+									name='paymentMethod'
+									value='Stripe'
+									checked={formData.paymentMethod === "Stripe"}
+									onChange={handleChange}
+									className='mr-2'
+								/>
+								Stripe
+							</label>
+						</div>
 
-				<button
-					type='submit'
-					className='bg-indigo-600 text-white p-2 rounded mt-4 w-full hover:opacity-90'
-				>
-					Place Order
-				</button>
-			</form>
+						<button
+							type='submit'
+							className='bg-indigo-600 text-white p-2 rounded mt-4 w-full hover:opacity-90'
+						>
+							Place Order
+						</button>
+					</form>
+				</div>
+			)}
 		</div>
 	);
 };
