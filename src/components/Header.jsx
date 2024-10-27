e1d6d5e07ea4e73c8bb24915669e189f10192507
umairahmed88 <umairahmed_88@ymail.com>
2024-10-27 05:36:09 +0500

    Updated header for admin and users

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-const links = [
-	{ name: "Home", to: "/" },
-	{ name: "Products", to: "/products" },
+const userLinks = [
 	{ name: "Cart", to: "/cart-items" },
+	{ name: "Orders", to: "/orders" },
+];
+
+const adminLinks = [
+	{ name: "Products", to: "/products" },
 	{ name: "Orders", to: "/orders" },
 ];
 
@@ -21,17 +24,32 @@ const Header = () => {
 					UA Store
 				</Link>
 				<nav className='flex items-center gap-6'>
+					<Link className='hover:opacity-90 transition duration-300' to={"/"}>
+						Home
+					</Link>
 					{currentUser ? (
 						<>
-							{links.map((link) => (
-								<Link
-									className='hover:opacity-90 transition duration-300'
-									to={link.to}
-									key={link.to}
-								>
-									{link.name}
-								</Link>
-							))}
+							{currentUser.sanitizedUser.role === "user"
+								? userLinks.map((link) => (
+										<Link
+											className='hover:opacity-90 transition duration-300'
+											to={link.to}
+											key={link.to}
+										>
+											{link.name}
+										</Link>
+								  ))
+								: adminLinks.map((link) => (
+										<Link
+											className='hover:opacity-90 transition duration-300'
+											to={link.to}
+											key={link.to}
+										>
+											{link.name}
+										</Link>
+								  ))}
+
+							{/* Profile link */}
 							<Link to='/profile' className='flex items-center'>
 								<img
 									src={currentUser.sanitizedUser.avatar}
@@ -55,58 +73,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
-
-// const links = [
-// 	{ name: "Home", to: "/" },
-// 	{ name: "Products", to: "/products" },
-// 	{ name: "Cart", to: "/cart-items" },
-// 	// { name: "Signin", to: "/signin" },
-// ];
-
-// const Header = () => {
-// 	const { currentUser } = useSelector((state) => state.auth);
-// 	return (
-// 		<header className=' bg-indigo-600 items-center py-2 px-3 text-white shadow-md '>
-// 			<div className=' mx-auto flex justify-between items-center'>
-// 				<Link
-// 					to='/'
-// 					className='text-xl font-bold tracking-wid hover:opacity-90'
-// 				>
-// 					UA Store
-// 				</Link>
-// 				<nav className='flex items-center gap-6'>
-// 					{links.map((link) => (
-// 						<Link
-// 							className='hover:opacity-90 transition duration-300'
-// 							to={link.to}
-// 							key={link.to}
-// 						>
-// 							{link.name}
-// 						</Link>
-// 					))}
-// 					{currentUser ? (
-// 						<Link to='/profile' className='flex items-center'>
-// 							<img
-// 								src={currentUser.sanitizedUser.avatar}
-// 								alt='Profile'
-// 								className='h-8 w-8 rounded-full'
-// 							/>
-// 						</Link>
-// 					) : (
-// 						<Link
-// 							to='/signin'
-// 							className='rounded-lg transition duration-300 hover:opacity-90'
-// 						>
-// 							Sign In
-// 						</Link>
-// 					)}
-// 				</nav>
-// 			</div>
-// 		</header>
-// 	);
-// };
-
-// export default Header;
