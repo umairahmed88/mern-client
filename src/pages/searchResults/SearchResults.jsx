7ea7a0b5e4454dc65b055bb3a8655379050ec6b0
umairahmed88 <umairahmed_88@ymail.com>
2024-10-27 20:33:05 +0500

    updated search product component

--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -6,11 +6,13 @@ const SearchResults = () => {
 		(state) => state.products
 	);
 
+	{
+		searchLoading && <p>Loading...</p>;
+	}
+
 	return (
 		<div className='container mx-auto px-4 py-8'>
 			<h2 className='text-2xl font-semibold mb-4'>Search Results</h2>
-
-			{searchLoading && <p>Loading...</p>}
 
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
 				{searchResults && searchResults.length > 0 ? (
