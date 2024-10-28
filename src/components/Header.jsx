9be0a03504e291a67582ab7f6a2243154a865aeb
umairahmed88 <umairahmed_88@ymail.com>
2024-10-28 15:10:50 +0500

    added menu bar to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { MdMenu, MdClose } from "react-icons/md";
 import Search from "./Search";
 
 const userLinks = [
@@ -14,9 +16,13 @@ const adminLinks = [
 
 const Header = () => {
 	const { currentUser } = useSelector((state) => state.auth);
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	// Toggle menu function
+	const toggleMenu = () => setMenuOpen(!menuOpen);
 
 	return (
-		<header className='bg-indigo-600 items-center py-2 px-3 text-white shadow-md'>
+		<header className='bg-indigo-600 py-2 px-3 text-white shadow-md'>
 			<div className='mx-auto flex justify-between items-center'>
 				<NavLink
 					to='/'
@@ -27,7 +33,15 @@ const Header = () => {
 
 				<Search />
 
-				<nav className='flex items-center gap-6'>
+				<div className='lg:hidden'>
+					{menuOpen ? (
+						<MdClose onClick={toggleMenu} className='h-6 w-6 cursor-pointer' />
+					) : (
+						<MdMenu onClick={toggleMenu} className='h-6 w-6 cursor-pointer' />
+					)}
+				</div>
+
+				<nav className='hidden lg:flex items-center gap-6'>
 					<NavLink
 						to='/'
 						className={({ isActive }) =>
@@ -85,6 +99,74 @@ const Header = () => {
 					)}
 				</nav>
 			</div>
+
+			{menuOpen && (
+				<nav className='lg:hidden flex flex-col gap-4 mt-3'>
+					<NavLink
+						to='/'
+						onClick={toggleMenu}
+						className={({ isActive }) =>
+							`transition duration-200 rounded-lg p-1 ${
+								isActive ? "bg-indigo-700" : "hover:bg-indigo-500"
+							}`
+						}
+					>
+						Home
+					</NavLink>
+					{currentUser ? (
+						<>
+							{currentUser.sanitizedUser.role === "user"
+								? userLinks.map((link) => (
+										<NavLink
+											to={link.to}
+											key={link.to}
+											onClick={toggleMenu}
+											className={({ isActive }) =>
+												`transition duration-200 rounded-lg p-1 ${
+													isActive ? "bg-indigo-700" : "hover:bg-indigo-500"
+												}`
+											}
+										>
+											{link.name}
+										</NavLink>
+								  ))
+								: adminLinks.map((link) => (
+										<NavLink
+											to={link.to}
+											key={link.to}
+											onClick={toggleMenu}
+											className={({ isActive }) =>
+												`transition duration-200 rounded-lg p-1 ${
+													isActive ? "bg-indigo-700" : "hover:bg-indigo-500"
+												}`
+											}
+										>
+											{link.name}
+										</NavLink>
+								  ))}
+							<NavLink
+								to='/profile'
+								onClick={toggleMenu}
+								className='flex items-center p-1 rounded-lg hover:bg-indigo-500'
+							>
+								<img
+									src={currentUser.sanitizedUser.avatar}
+									alt='Profile'
+									className='h-8 w-8 rounded-full'
+								/>
+							</NavLink>
+						</>
+					) : (
+						<NavLink
+							to='/signin'
+							onClick={toggleMenu}
+							className='transition duration-200 rounded-lg hover:bg-indigo-500 p-1'
+						>
+							Sign In
+						</NavLink>
+					)}
+				</nav>
+			)}
 		</header>
 	);
 };
@@ -92,7 +174,8 @@ const Header = () => {
 export default Header;
 
 // import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+// import Search from "./Search";
 
 // const userLinks = [
 // 	{ name: "Cart", to: "/cart-items" },
@@ -108,56 +191,72 @@ export default Header;
 // 	const { currentUser } = useSelector((state) => state.auth);
 
 // 	return (
-// 		<header className='bg-indigo-600 items-center py-2 px-3 text-white shadow-md '>
+// 		<header className='bg-indigo-600 items-center py-2 px-3 text-white shadow-md'>
 // 			<div className='mx-auto flex justify-between items-center'>
-// 				<Link
+// 				<NavLink
 // 					to='/'
-// 					className='text-xl font-bold tracking-wid hover:opacity-90'
+// 					className='text-xl font-bold tracking-wide hover:bg-indigo-500 p-1 rounded-lg'
 // 				>
 // 					UA Store
-// 				</Link>
+// 				</NavLink>
+
+// 				<Search />
+
 // 				<nav className='flex items-center gap-6'>
-// 					<Link className='hover:opacity-90 transition duration-300' to={"/"}>
+// 					<NavLink
+// 						to='/'
+// 						className={({ isActive }) =>
+// 							`transition duration-200 rounded-lg p-1 ${
+// 								isActive ? "bg-indigo-700" : " hover:bg-indigo-500"
+// 							}`
+// 						}
+// 					>
 // 						Home
-// 					</Link>
+// 					</NavLink>
 // 					{currentUser ? (
 // 						<>
 // 							{currentUser.sanitizedUser.role === "user"
 // 								? userLinks.map((link) => (
-// 										<Link
-// 											className='hover:opacity-90 transition duration-300'
+// 										<NavLink
 // 											to={link.to}
 // 											key={link.to}
+// 											className={({ isActive }) =>
+// 												`transition duration-200 rounded-lg p-1 ${
+// 													isActive ? "bg-indigo-700" : "hover:bg-indigo-500"
+// 												}`
+// 											}
 // 										>
 // 											{link.name}
-// 										</Link>
+// 										</NavLink>
 // 								  ))
 // 								: adminLinks.map((link) => (
-// 										<Link
-// 											className='hover:opacity-90 transition duration-300'
+// 										<NavLink
 // 											to={link.to}
 // 											key={link.to}
+// 											className={({ isActive }) =>
+// 												`transition duration-200 rounded-lg p-1 ${
+// 													isActive ? "bg-indigo-700" : "hover:bg-indigo-500"
+// 												}`
+// 											}
 // 										>
 // 											{link.name}
-// 										</Link>
+// 										</NavLink>
 // 								  ))}
-
-// 							{/* Profile link */}
-// 							<Link to='/profile' className='flex items-center'>
+// 							<NavLink to='/profile' className='flex items-center'>
 // 								<img
 // 									src={currentUser.sanitizedUser.avatar}
 // 									alt='Profile'
 // 									className='h-8 w-8 rounded-full'
 // 								/>
-// 							</Link>
+// 							</NavLink>
 // 						</>
 // 					) : (
-// 						<Link
+// 						<NavLink
 // 							to='/signin'
-// 							className='rounded-lg transition duration-300 hover:opacity-90'
+// 							className='transition duration-200 rounded-lg hover:bg-indigo-500 p-1'
 // 						>
 // 							Sign In
-// 						</Link>
+// 						</NavLink>
 // 					)}
 // 				</nav>
 // 			</div>
